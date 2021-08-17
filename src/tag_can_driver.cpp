7fa43e257da3a3dff3e05a62ba9a3556a7607b4d
urasaki <213427006@ccmailg.meijo-u.ac.jp>
2021-08-17 10:12:50 +0900

    add diag and config for fog

--- src/tag_can_driver.cpp
+++ src/tag_can_driver.cpp
@@ -32,9 +32,15 @@
 #include "ros/ros.h"
 #include "can_msgs/Frame.h"
 #include "sensor_msgs/Imu.h"
+#include "diagnostic_updater/diagnostic_updater.h"
 
 static unsigned int counter;
 static int16_t raw_data;
+static int32_t raw_data_fog;
+static uint16_t imu_status;
+static bool use_fog;
+
+static diagnostic_updater::Updater* p_updater;
 
 static sensor_msgs::Imu imu_msg;
 static ros::Publisher pub;
@@ -44,7 +50,7 @@ void receive_can_callback(const can_msgs::Frame::ConstPtr& msg){
   if(msg->id == 0x319)
   {
     imu_msg.header.frame_id = "imu";
-    imu_msg.header.stamp = ros::Time::now();
+    imu_msg.header.stamp = msg->header.stamp;
 
     counter = msg->data[1] + (msg->data[0] << 8);
     raw_data = msg->data[3] + (msg->data[2] << 8);
@@ -53,9 +59,20 @@ void receive_can_callback(const can_msgs::Frame::ConstPtr& msg){
     raw_data = msg->data[5] + (msg->data[4] << 8);
     imu_msg.angular_velocity.y =
         raw_data * (200 / pow(2, 15)) * M_PI / 180;  // LSB & unit [deg/s] => [rad/s]
-    raw_data = msg->data[7] + (msg->data[6] << 8);
-    imu_msg.angular_velocity.z =
+    
+    if (use_fog) 
+    {
+      raw_data_fog = (msg->data[7] + (msg->data[6] << 8)) + ((msg->data[5] << 16) + (msg->data[4] << 24));
+      imu_msg.angular_velocity.z =
+        raw_data_fog * (200 / pow(2, 15)) * M_PI / 180;  // LSB & unit [deg/s] => [rad/s]
+    }
+    else 
+    {
+      raw_data = msg->data[7] + (msg->data[6] << 8);
+      imu_msg.angular_velocity.z =
         raw_data * (200 / pow(2, 15)) * M_PI / 180;  // LSB & unit [deg/s] => [rad/s]
+    }
+    
   }
   if(msg->id == 0x31A)
   {
@@ -74,14 +91,43 @@ void receive_can_callback(const can_msgs::Frame::ConstPtr& msg){
     //std::cout << counter << std::endl;
   }
 
+  p_updater->update();
+
+}
+
+void diagnostic_tag_can(diagnostic_updater::DiagnosticStatusWrapper& stat)
+{
+  ros::Time diagnostic_time = ros::Time::now();
+
+  if(diagnostic_time.toSec() - imu_msg.header.stamp.toSec() >= 5.0)
+  {
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::STALE, "No data coming in for more than 1s");
+  }
+  else if(imu_status & 0x8000)
+  {
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::ERROR, "Error detected by BIT");
+  }
+  else
+  {
+    stat.summaryf(diagnostic_msgs::DiagnosticStatus::OK, "OK");
+  }
 }
 
 int main(int argc, char **argv){
 
   ros::init(argc, argv, "tag_can_driver");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("/imu/can_tx", 100, receive_can_callback);
-  pub = n.advertise<sensor_msgs::Imu>("/imu/data_raw", 100);
+  ros::NodeHandle pnh("~");
+
+  pnh.getParam("tag_can_driver/use_fog", use_fog);
+
+  diagnostic_updater::Updater updater;
+  p_updater = &updater;
+  updater.setHardwareID("tamagawa_imu");
+  updater.add("tamagawa_imu", diagnostic_tag_can);
+  
+  ros::Subscriber sub = n.subscribe("imu/can_tx", 100, receive_can_callback);
+  pub = n.advertise<sensor_msgs::Imu>("imu/data_raw", 100);
   ros::spin();
 
   return 0;
